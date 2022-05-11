468a7833d5c6a4e76e55decb9642445691c74498
Rouven Grenz <rouven.grenz@gmx.net>
2022-05-11 14:35:01 +0000

    Cleanup

--- scripts/1b_RunFusekiServer.sh
+++ scripts/1b_RunFusekiServer.sh
@@ -7,4 +7,5 @@ rdflimeUtil=$scriptsRoot/..
 echo "Running Apache Jena Fuseki server. You may want to forward port 3030 in case you are in a docker environment."
 
 # Without sudo it sometimes throws cryptic erros
-sudo $rdflimeUtil/jena/apache-jena-fuseki-4.4.0/fuseki-server --loc $rdflimeUtil/jena/apache-jena-fuseki-4.4.0/run/datasets/dbpedia /dbpedia
+cd $rdflimeUtil/jena/apache-jena-fuseki-4.4.0
+sudo ./fuseki-server --loc ./run/datasets/dbpedia /dbpedia
